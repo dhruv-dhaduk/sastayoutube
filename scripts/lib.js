c1139977452e263b632f88ab02676d290240019f
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-10-18 09:26:02 +0530

    Changed the directory structure for stylesheets and scripts

--- scripts/lib.js
+++ scripts/lib.js
@@ -95,3 +95,12 @@ function time_relative_to(timediff, unitTime, label)
     else
         return x + " " + label + " ago";
 }
+
+function shuffle(array) 
+{
+    for (let i = array.length - 1; i > 0; i--) 
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
