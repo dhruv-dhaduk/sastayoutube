723f2a717d47b75608112c80d66d2b994878f9d6
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-10-20 15:17:36 +0530

    Created javascript functions to add dummy data to template in yt desktop

--- scripts/desktop/loader.js
+++ scripts/desktop/loader.js
@@ -2,3 +2,83 @@ function load_data()
 {
     return;
 }
+
+const data = [];
+for (r of hardCodedData) {
+    data.push(r);
+}
+load_data();
+
+function load_data() 
+{
+    const feed = document.querySelector("#video-feed");
+    for (const r of data) 
+    {
+        create_html_video_item(r);
+        feed.append(r["htmlItem"]);
+    }
+}
+
+function create_html_video_item(v) 
+{
+    const middle_dot = " <span class=\"dot\">&#183</span> ";
+
+    const videoItem = document.createElement("div");
+        const thumbnailContainer = document.createElement("div");
+            const thumbnailImg = document.createElement("img");
+            const duration = document.createElement("p");
+        const videoItemInfo = document.createElement("div");
+            const channelIconContainer = document.createElement("a");
+                const channelIcon = document.createElement("img");
+            const videoItemTexts = document.createElement("div");
+                const videoTitle = document.createElement("p");
+                const videoChannelName = document.createElement("p");
+                const videoAdditionalData = document.createElement("p");
+            const threeDotsContainer = document.createElement("a");
+                const threeDots = document.createElement("img");
+
+    videoItem.className = "video-item";
+        thumbnailContainer.className = "thumbnail-container";
+            thumbnailImg.className = "thumbnail-img";
+            thumbnailImg.src = v["thumbnail"];
+            thumbnailImg.addEventListener("contextmenu", function(e) { e.preventDefault(); });
+            duration.className = "duration";
+            duration.innerHTML = v["duration"];
+        videoItemInfo.className = "video-item-info";
+            channelIconContainer.className = "channel-icon-container";
+            channelIconContainer.href = v["channelLink"];
+            channelIconContainer.target = "_blank";
+                channelIcon.className = "channel-icon";
+                channelIcon.src = v["channelIcon"];
+                channelIcon.addEventListener("contextmenu", function(e) { e.preventDefault(); });
+            videoItemTexts.className = "video-item-texts";
+                videoTitle.className = "video-title";
+                videoTitle.innerHTML = v["videoTitle"];
+                videoChannelName.className = "video-channel-name";
+                videoChannelName.innerHTML = v["channelTitle"];
+                videoAdditionalData.className = "video-additional-data";
+                videoAdditionalData.innerHTML = convert_number_format(v["viewCount"], "views") + middle_dot + convert_upload_time_format(v["uploadTime"]);
+            threeDotsContainer.className = "video-three-dots-container";
+            threeDotsContainer.href = v["link"];
+            threeDotsContainer.target = "_blank";
+                threeDots.className = "video-three-dots";
+                threeDots.src = "https://dhruv-dhaduk.github.io/assets/logos/white/three_dots_white.png";
+                threeDots.addEventListener("contextmenu", function(e) { e.preventDefault(); });
+
+    channelIconContainer.append(channelIcon);
+    videoItemTexts.append(videoTitle);
+    videoItemTexts.append(videoChannelName);
+    videoItemTexts.append(videoAdditionalData);
+    threeDotsContainer.append(threeDots);
+    thumbnailContainer.append(thumbnailImg);
+    thumbnailContainer.append(duration);
+    videoItemInfo.append(channelIconContainer);
+    videoItemInfo.append(videoItemTexts);
+    videoItemInfo.append(threeDotsContainer);
+    videoItem.append(thumbnailContainer);
+    videoItem.append(videoItemInfo);
+
+    v["htmlItem"] = videoItem;
+
+    console.log(v);
+}
