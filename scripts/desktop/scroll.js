e75c71ba7f691fbdb15d4f505561d497550e13a0
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-10-22 16:57:48 +0530

    Fixed a bug in shorts scrolling in yt desktop

--- scripts/desktop/scroll.js
+++ scripts/desktop/scroll.js
@@ -10,28 +10,32 @@ check_max_shorts_scroll();
 
 function shortslist_left_scroll() {
     if (shortslist.scrollLeft - shortslist.offsetWidth / 2 <= 100)
-        shortslist.scrollTo(0, 0);
-    else
-        shortslist.scrollTo(shortslist.scrollLeft - shortslist.offsetWidth / 2, 0);
+    shortslist.scrollTo(0, 0);
+else
+shortslist.scrollTo(shortslist.scrollLeft - shortslist.offsetWidth / 2, 0);
 
-    setTimeout(check_max_shorts_scroll, 1000);
+setTimeout(check_max_shorts_scroll, 1000);
 }
 
-function shortslist_right_scroll() {
-    if (shortslist.scrollLeftMax - shortslist.scrollLeft - shortslist.offsetWidth / 2 <= 100)
-        shortslist.scrollTo(shortslist.scrollLeftMax, 0);
-    else
-        shortslist.scrollTo(shortslist.scrollLeft + shortslist.offsetWidth / 2, 0);
+function shortslist_right_scroll() {    
+    const shortslistScrollLeftMax = shortslist.scrollWidth - shortslist.offsetWidth;
+    
+    if (shortslistScrollLeftMax - shortslist.scrollLeft - shortslist.offsetWidth / 2 <= 100)
+    shortslist.scrollTo(shortslistScrollLeftMax, 0);
+else
+shortslist.scrollTo(shortslist.scrollLeft + shortslist.offsetWidth / 2, 0);
 
-    setTimeout(check_max_shorts_scroll, 1000);
+setTimeout(check_max_shorts_scroll, 1000);
 }
 
 function check_max_shorts_scroll() {
+    const shortslistScrollLeftMax = shortslist.scrollWidth - shortslist.offsetWidth;
+
     if (shortslist.scrollLeft === 0) {
         shortsleftscroll.style.opacity = 0;
         shortsrightscroll.style.opacity = 1;
     }
-    else if (shortslist.scrollLeft === shortslist.scrollLeftMax) {
+    else if (Math.abs(shortslist.scrollLeft - shortslistScrollLeftMax) <= 5) {
         shortsleftscroll.style.opacity = 1;
         shortsrightscroll.style.opacity = 0;
     }
