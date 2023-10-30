5c53662cb0a9903c5de14f486ade3add96078916
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-10-30 22:54:04 +0530

    Removed the commented code

--- scripts/mobile/main.js
+++ scripts/mobile/main.js
@@ -10,23 +10,6 @@ if(!isMobileDevice())
 
 start_fetching();
 
-// let data;
-
-// const fetchWorker = new Worker("./scripts/fetch_worker.js", {type: "module"});
-// fetchWorker.addEventListener("message", function(msg) {
-//     if (msg.data.error) {
-//         const err = `Error while fetching data :${msg.data.error}`;
-//         console.log(err);
-//         alert(err);
-//     }
-//     else {
-//         data = msg.data.videos;
-//         console.log(data);
-//         load_data(data);
-//     }
-// });
-// fetchWorker.postMessage({command: "fetchAll"});
-
 document.addEventListener("DOMContentLoaded", function() {
     init();
 });
