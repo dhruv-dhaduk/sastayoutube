995b2f6d7dea106d6e4f50b4d2a2fecdab12964e
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-10-30 19:49:16 +0530

    Created a function to wrap the fetch worker initialization in data.js in yt desktop

--- scripts/desktop/data.js
+++ scripts/desktop/data.js
@@ -2,17 +2,21 @@ import { load_data } from "./loader.js";
 
 export let data;
 
-const fetchWorker = new Worker("./scripts/fetch_worker.js", {type: "module"});
-fetchWorker.addEventListener("message", function(msg) {
-    if (msg.data.error) {
-        const err = `Erro while fetching data : ${msg.data.error}`;
-        console.log(err);
-        alert(err);
-    }
-    else {
-        data = msg.data.videos;
-        console.log(data);
-        load_data(data);
-    }
-});
-fetchWorker.postMessage({command: "fetchAll"});
+export function start_fetching()
+{
+    const fetchWorker = new Worker("./scripts/fetch_worker.js", {type: "module"});
+    fetchWorker.addEventListener("message", function(msg) {
+        if (msg.data.error) {
+            const err = `Erro while fetching data : ${msg.data.error}`;
+            console.log(err);
+            alert(err);
+        }
+        else {
+            data = msg.data.videos;
+            console.log(data);
+            load_data(data);
+        }
+        fetchWorker.terminate();
+    });
+    fetchWorker.postMessage({command: "fetchAll"});
+}
--- scripts/desktop/main.js
+++ scripts/desktop/main.js
@@ -1,36 +1,14 @@
 import { isMobileDevice } from "../device.js";
-// import { load_data } from "./loader.js";
 import { init } from "./init.js";
+import { start_fetching } from "./data.js";
 
 if (isMobileDevice())
 {
     window.location.href = "youtubeM.html";
 }
 
-// let data;
-
-// const fetchWorker = new Worker("./scripts/fetch_worker.js", {type: "module"});
-// fetchWorker.addEventListener("message", function(msg) {
-//     if (msg.data.error) {
-//         const err = `Erro while fetching data : ${msg.data.error}`;
-//         console.log(err);
-//         alert(err);
-//     }
-//     else {
-//         data = msg.data.videos;
-//         console.log(data);
-//         load_data(data);
-//     }
-// });
-// fetchWorker.postMessage({command: "fetchAll"});
+start_fetching();
 
 document.addEventListener("DOMContentLoaded", function() {
-    // document.querySelector("#yt").addEventListener("click", function() {
-    //     refreshVideoList(data, false);
-    // });
-    
-    // document.querySelector("#shuffle").addEventListener("click", function() {
-    //     refreshVideoList(data, true);
-    // });
     init();
 });
