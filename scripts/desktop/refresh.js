35ae690ff0c86f635b04f7454468cbc74fde4d80
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-11-05 12:22:12 +0530

    Structured both the refresh.js properly

--- scripts/desktop/refresh.js
+++ scripts/desktop/refresh.js
@@ -2,16 +2,20 @@ import { shuffle } from "../lib.js";
 import { check_max_shorts_scroll } from "./scroll.js";
 import { playingVideoData } from "./player.js";
 
-var videoslistITV, shortslistITV;
+let videoslistITV, shortslistITV;
+
 export function refreshVideoList(data, doShuffle)
 {
     if (doShuffle)
         shuffle(data);
 
+    const videolist = document.querySelector("#video-feed");
+    
     const shortsfeed = document.querySelector("#shorts-feed");
     const shortslist = document.querySelector("#shorts-list");
-    const videolist = document.querySelector("#video-feed");
+    
     videolist.innerHTML = "";
+    
     shortslist.innerHTML = "";
     check_max_shorts_scroll();
     videolist.append(shortsfeed);
@@ -24,30 +28,30 @@ export function refreshVideoList(data, doShuffle)
     clearInterval(videoslistITV);
     clearInterval(shortslistITV);
 
-    var videosCt = 0;
+    let videosCt = 0;
     videoslistITV = setInterval(() => {
-        while ((videosCt < data.length) && (data[videosCt]["status"] != "done" || data[videosCt]["type"] != "video"))
+        while ((videosCt < data.length) && (data[videosCt].status != "done" || data[videosCt].type != "video"))
             videosCt++;
 
         if (videosCt >= data.length) {
             clearInterval(videoslistITV);
         }
         else {
-            videolist.append(data[videosCt]["htmlItem"]);
+            videolist.append(data[videosCt].htmlItem);
             videosCt++;
         }
     }, 100);
 
-    var shortsCt = 0;
+    let shortsCt = 0;
     shortslistITV = setInterval(() => {
-        while ((shortsCt < data.length) && (data[shortsCt]["status"] != "done" || data[shortsCt]["type"] != "short"))
+        while ((shortsCt < data.length) && (data[shortsCt].status != "done" || data[shortsCt].type != "short"))
             shortsCt++;
 
         if (shortsCt >= data.length) {
             clearInterval(shortslistITV);  
         }
         else {
-            shortslist.append(data[shortsCt]["htmlItem"]);
+            shortslist.append(data[shortsCt].htmlItem);
             shortsCt++;
         }
         
--- scripts/mobile/refresh.js
+++ scripts/mobile/refresh.js
@@ -1,14 +1,14 @@
 import { shuffle } from "../lib.js";
 import { create_shorts_feed } from "./loader.js";
 
-var videoslistITV, shortslistITV;
+let videoslistITV, shortslistITV;
 
 export function refreshVideoList(data, doShuffle)
 {
     if (doShuffle)
         shuffle(data);
 
-    const videoslist = document.getElementById("video-list");
+    const videoslist = document.querySelector("#video-list");
     videoslist.innerHTML = "";
 
     const shortsfeedList = create_shorts_feed();
@@ -18,35 +18,36 @@ export function refreshVideoList(data, doShuffle)
     clearInterval(videoslistITV);
     clearInterval(shortslistITV);
     
-    var videosCt = 0;
+    let videosCt = 0;
     videoslistITV = setInterval(() => {
         if (videoslist.childElementCount == 1)
             videoslist.append(shortsfeed);
 
-        while ((videosCt < data.length) && (data[videosCt]["status"] != "done" || data[videosCt]["type"] != "video"))
+        while ((videosCt < data.length) && (data[videosCt].status != "done" || data[videosCt].type != "video"))
         videosCt++;
     
         if (videosCt >= data.length) {
             clearInterval(videoslistITV);
-            return;
         }
-        
-        videoslist.append(data[videosCt]["htmlItem"]);
-        videosCt++;
+        else {
+            videoslist.append(data[videosCt].htmlItem);
+            videosCt++;
+        }
     }, 100);
 
-    var shortsCt = 0;
+    let shortsCt = 0;
     shortslistITV = setInterval(() => {
-        while ((shortsCt < data.length) && (data[shortsCt]["status"] != "done" || data[shortsCt]["type"] != "short"))
+        while ((shortsCt < data.length) && (data[shortsCt].status != "done" || data[shortsCt].type != "short"))
             shortsCt++;
 
         if (shortsCt >= data.length) {
             clearInterval(shortslistITV);
-            return;
+        }
+        else {
+            shortslist.append(data[shortsCt].htmlItem);
+            shortsCt++;
         }
 
-        shortslist.append(data[shortsCt]["htmlItem"]);
-        shortsCt++;
     }, 100);
 
 }
